1d892a39a7aef91a694d052ae1def7fdcc13ded5
Joy <joymridha939@gmail.com>
2024-04-20 13:15:47 +0530

    added logo and changed routes

--- server.js
+++ server.js
@@ -1,37 +1,42 @@
-import express from 'express';
-import path from 'path';
-import multer from 'multer';
-import PDFMerger from 'pdf-merger-js'
-const upload = multer({ dest: 'uploads/' })
+import express from "express";
+import path from "path";
+import multer from "multer";
+import PDFMerger from "pdf-merger-js";
+import { root } from "postcss";
+const upload = multer({ dest: "uploads/" });
 const app = express();
 const port = 3000;
 
 const mergePdfs = async (p) => {
-  var merger = new PDFMerger();
+	var merger = new PDFMerger();
 
-  for (let i = 0; i < p.length; i++) {
-    await merger.add(p[i].path);
-  }
+	for (let i = 0; i < p.length; i++) {
+		await merger.add(p[i].path);
+	}
 
-  let n = new Date().getTime();
-  await merger.save(`public/merged_${n}.pdf`);
+	let n = new Date().getTime();
+	await merger.save(`public/merged_${n}.pdf`);
 
-  return n;
+	return n;
 };
 
-
-app.use('/static', express.static('public'))
-app.use(express.static('templates'));
+app.use("/static", express.static("public"));
+app.use("/static", express.static("uploads"));
+app.use(express.static("src"));
 
 app.get("/", (req, res) => {
-  res.sendFile("templates/index.html");
+	res.sendFile("templates/index.html", { root: "./src" });
 });
 
-app.post('/merge', upload.array('pdfs', 12), async (req, res, next) => {
-  let n = await mergePdfs(req.files);
-  res.redirect(`http://localhost:${port}/static/merged_${n}.pdf`);
-})
+app.get("/merge", (req, res) => {
+	res.sendFile("templates/merge.html", { root: "./src" });
+});
+
+app.post("/merge", upload.array("pdfs", 12), async (req, res, next) => {
+	let n = await mergePdfs(req.files);
+	res.redirect(`http://localhost:${port}/static/merged_${n}.pdf`);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
+	console.log(`Example app listening on port http://localhost:${port}`);
 });
